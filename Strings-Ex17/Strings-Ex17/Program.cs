571d350c104c377fdb65642aac2dc8d6256ed3f3
MariusA <anca.ticau@gmail.com>
2019-06-10 23:16:25 +0300

    Strings-Ex17

---
+++ Strings-Ex17/Strings-Ex17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Strings_Ex17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "This is an awesome string";
+            Console.WriteLine(RemoveSpaces(givenString));
+        }
+        //Write a method to remove spaces from a given string.
+
+        public static string RemoveSpaces(string input)
+        {
+            string replaced = input.Replace(" ", "");
+
+            return replaced;
+        }
+    }
+}
